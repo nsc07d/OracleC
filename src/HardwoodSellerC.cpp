fd1d56da87a6ad1aca5ffff86ff528ebf196e20a
nsc07d <nsc07d@my.fsu.edu>
2016-01-27 22:52:32 -0500

    Update HardWoodSellerC.cpp
    This change I implemented the getline and line parser (buyerInfo() function). This simply
    reads in the first line and grabs the info for Buyer, Address, and Date.

--- src/HardwoodSellerC.cpp
+++ src/HardwoodSellerC.cpp
@@ -26,6 +26,17 @@ int main() {
  */
 void readInputFile(string inputFilePath) {
 
+	//First line placeholder strings
+	string firstLine, buyer, address, date, temp;
+	int count = 0;
+
+	//Open file and get first line for parsing
+	ifstream inFile;
+	inFile.open(inputFilePath);
+	getline(inFile, firstLine);
+
+	//Function to parse just the first line
+	buyerInfo(firstLine, count, buyer, address, date, temp);
 }
 
 /*
@@ -34,4 +45,40 @@ void readInputFile(string inputFilePath) {
 double deliveryTime() {
 	double deliveryETA = 0.0;
 	return deliveryETA;
+}
+
+/*
+* Simply parses the top line. Known amount of tokens makes this easy
+*/
+void buyerInfo(string& x, int y, string& buyer, string& address, string& date, string& temp)
+{
+	//Parses the entire line
+	for (unsigned int i = 0; i < x.length(); i++)
+	{
+		//If you reach a ; and count == 0 you've reached the name of the buyer
+		if (x[i] == ';' && y == 0)
+		{
+			buyer = temp;
+			temp = "";
+			y++;
+		}
+		//Otherwise if you reach a ; and count == 1 you've reached the end of the address
+		else if (x[i] == ';' && y == 1)
+		{
+			address = temp;
+			temp = "";
+			y++;
+		}
+		//Otherwise you just append line[i] to a string until it reaches a ;
+		else
+			temp += x[i];
+	}
+	//Setting the remainder of the temp string to date since that's what's left over
+	date = temp;
+
+	//This resets temp and x for parsing the second line
+	temp = "";
+	x = "";
+
+
 }
